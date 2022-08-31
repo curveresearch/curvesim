8562334c718da3d7eb9e63ca87c0b3fe84f58c61
Chan-Ho Suh <chanhosuh@gmail.com>
2022-08-31 16:49:14 -0400

    Upload latest profiling results

--- hello_world.py
+++ hello_world.py
@@ -2,9 +2,8 @@ import CurveSim
 import time
 
 if __name__ == "__main__":
-    # res = CurveSim.autosim("3pool", fee=[0.0003, 0.0004], ncpu=1)
-    # res = CurveSim.autosim("3pool", fee=[0.0003], ncpu=1)
     t = time.time()
     res = CurveSim.autosim("3pool", test=True, ncpu=1)
+    res = CurveSim.autosim("aave", test=True, ncpu=1)
     elapsed = time.time() - t
     print("Elapsed time:", elapsed)
--- hello_world_gmp.py
+++ hello_world_gmp.py
@@ -4,5 +4,6 @@ import time
 if __name__ == "__main__":
     t = time.time()
     res = CurveSimGmp.autosim("3pool", test=True, ncpu=1)
+    res = CurveSimGmp.autosim("aave", test=True, ncpu=1)
     elapsed = time.time() - t
     print("Elapsed time:", elapsed)
