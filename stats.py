c15977845c4cee41b00b2513c874746031ac95cc
Chan-Ho Suh <chanhosuh@gmail.com>
2022-09-05 17:33:39 -0400

    Delete profile stats script

--- stats.py
+++
@@ -1,9 +0,0 @@
-import pstats
-from pstats import SortKey
-
-p = pstats.Stats('output.txt')
-p.sort_stats(SortKey.CUMULATIVE).print_stats(25)
-
-p = pstats.Stats('output_gmp.txt')
-p.sort_stats(SortKey.CUMULATIVE).print_stats(25)
-
