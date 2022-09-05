c8d25257a1ed2c5085d3fa65cc1b6275bfd3a56f
Chan-Ho Suh <chanhosuh@gmail.com>
2022-09-05 17:31:14 -0400

    Remove aux hellow world

--- hello_world_gmp.py
+++
@@ -1,9 +0,0 @@
-import CurveSimGmp
-import time
-
-if __name__ == "__main__":
-    t = time.time()
-    res = CurveSimGmp.autosim("3pool", test=True, ncpu=1)
-    res = CurveSimGmp.autosim("aave", test=True, ncpu=1)
-    elapsed = time.time() - t
-    print("Elapsed time:", elapsed)
