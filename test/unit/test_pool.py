1ed783dc551efdafe25044aafe3f221bac7efa80
Chan-Ho Suh <chanhosuh@gmail.com>
2022-09-26 14:40:08 -0400

    Test our D calc

--- test/unit/test_pool.py
+++ test/unit/test_pool.py
@@ -1,7 +1,9 @@
 import boa
 
+from curvesim.pool import Pool
 
-def test_get_D():
+
+def test_get_D_against_prod():
 
     vyper_pool = boa.load("test/unit/pool_calcs.vy")
     A = 2000
@@ -18,3 +20,18 @@ def test_get_D():
     # while `get_D` is internal in the contract.
     expected_virtual_price = 1022038799187029697
     assert virtual_price == expected_virtual_price
+
+
+def test_get_D():
+    vyper_pool = boa.load("test/unit/pool_calcs.vy")
+    A = 2000
+    balances = [
+        295949605740077243186725223,
+        284320067518878,
+        288200854907854,
+    ]
+    virtual_balances = [balances[0], balances[1] * 10**12, balances[2] * 10**12]
+    expected_D = vyper_pool.get_D(virtual_balances, A)
+    pool = Pool(A, D=balances, n=3, p=[10**18, 10**30, 10**30])
+    D = pool.D()
+    assert D == expected_D
