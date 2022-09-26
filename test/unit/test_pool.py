5caf54febe16d04d7735409c801fa16b381f97a4
Chan-Ho Suh <chanhosuh@gmail.com>
2022-09-26 17:35:17 -0400

    Use pytest fixtures (thanks fiddy)

--- test/unit/test_pool.py
+++ test/unit/test_pool.py
@@ -1,40 +1,38 @@
-import boa
-
 from curvesim.pool import Pool
 
 
-def test_get_D_against_prod():
+def _calc_virtual_price(D, total_supply):
+    return D * 10**18 // total_supply
+
+
+def test_get_D_against_prod(vyper_3pool, mainnet_3pool_state):
     """Test boa value against live contract."""
 
-    vyper_pool = boa.load("test/unit/pool_calcs.vy")
-    A = 2000
-    balances = [
-        295949605740077243186725223,
-        284320067518878 * 10**12,
-        288200854907854 * 10**12,
-    ]
-    D = vyper_pool.get_D(balances, A)
-    total_supply = 849743149250065202008212976
-    virtual_price = D * 10**18 // total_supply
+    virtual_balances = mainnet_3pool_state["virtual_balances"]
+    A = mainnet_3pool_state["A"]
+    total_supply = mainnet_3pool_state["lp_tokens"]
 
     # Compare against virtual price since that's exposed externally
     # while `get_D` is internal in the contract.
-    expected_virtual_price = 1022038799187029697
+    D = vyper_3pool.get_D(virtual_balances, A)
+    virtual_price = _calc_virtual_price(D, total_supply)
+
+    expected_virtual_price = mainnet_3pool_state["virtual_price"]
     assert virtual_price == expected_virtual_price
 
 
-def test_get_D():
+def test_get_D(vyper_3pool, mainnet_3pool_state):
     """Test D calculation against vyper implementation."""
 
-    vyper_pool = boa.load("test/unit/pool_calcs.vy")
-    A = 2000
-    balances = [
-        295949605740077243186725223,
-        284320067518878,
-        288200854907854,
-    ]
-    virtual_balances = [balances[0], balances[1] * 10**12, balances[2] * 10**12]
-    expected_D = vyper_pool.get_D(virtual_balances, A)
-    pool = Pool(A, D=balances, n=3, p=[10**18, 10**30, 10**30])
+    virtual_balances = mainnet_3pool_state["virtual_balances"]
+    A = mainnet_3pool_state["A"]
+    expected_D = vyper_3pool.get_D(virtual_balances, A)
+
+    balances = mainnet_3pool_state["balances"]
+    p = mainnet_3pool_state["p"]
+    n_coins = mainnet_3pool_state["N_COINS"]
+
+    pool = Pool(A, D=balances, n=n_coins, p=p)
     D = pool.D()
+
     assert D == expected_D
