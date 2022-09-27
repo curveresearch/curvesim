022fe146b92790a74eaed476cb227c7c760841cc
Chan-Ho Suh <chanhosuh@gmail.com>
2022-09-27 12:10:11 -0400

    Setup erc20 mocks; fail virtual price test

--- test/conftest.py
+++ test/conftest.py
@@ -30,12 +30,22 @@ def mainnet_3pool_state():
 
 @pytest.fixture(scope="function")
 def vyper_3pool(mainnet_3pool_state):
-    filepath = os.path.join(_base_dir, "fixtures", "calcs_for_3pool.vy")
-    pool = boa.load(filepath)
+    erc20_filepath = os.path.join(_base_dir, "fixtures", "erc20_mock.vy")
+    lp_total_supply = mainnet_3pool_state["lp_tokens"]
+    lp_token = boa.load(erc20_filepath, "Mock 3CRV", "MOCK-3CRV", 18, lp_total_supply)
 
+    pool_filepath = os.path.join(_base_dir, "fixtures", "calcs_for_3pool.vy")
+    owner = "0xCAFECAFECAFECAFECAFECAFECAFECAFECAFECAFE"
+    dai_mock = boa.load(erc20_filepath, "Mock DAI", "MOCK-DAI", 18, 10**26).address
+    usdc_mock = boa.load(erc20_filepath, "Mock USDC", "MOCK-USDC", 6, 10**26).address
+    tether_mock = boa.load(erc20_filepath, "Mock Tether", "MOCK-UST", 6, 10**26).address
+    coins = [dai_mock, usdc_mock, tether_mock]
     A = mainnet_3pool_state["A"]
-    pool.eval(f"self.future_A={A}")
+    fee = 4 * 10**6
+    admin_fee = fee // 2
+    pool = boa.load(pool_filepath, owner, coins, lp_token, A, fee, admin_fee)
 
     balances = mainnet_3pool_state["balances"]
     pool.eval(f"self.balances={balances}")
+
     return pool
