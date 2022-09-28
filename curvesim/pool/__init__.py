d1323385318e17ed8acaf16c2a43a85bd0c058cd
nagakingg <nagakingg@protonmail.com>
2022-09-28 15:10:41 -0400

    Add pool.get and pool.make

--- curvesim/pool/__init__.py
+++ curvesim/pool/__init__.py
@@ -1,3 +1,26 @@
-__all__ = ["Pool"]
+__all__ = ["get", "make", "Pool"]
 
+from ..pool_data import get as _get_pool_data
 from .pool import Pool
+
+
+def make(A, D, n, p=None, tokens=None, fee=4 * 10**6, fee_mul=None, r=None):
+    pool = Pool(A, D, n, p=None, tokens=None, fee=4 * 10**6, fee_mul=None, r=None)
+
+    return pool
+
+
+def get(address_or_symbol, chain="mainnet", src="cg", balanced=(True, True)):
+    p = _get_pool_data(
+        address_or_symbol,
+        chain=chain,
+        src=src,
+        balanced=balanced,
+    )
+
+    A = [p["A"], p["A_base"]]
+    fee = [p["fee"], p["fee_base"]]
+
+    pool = make(A, p["D"], p["n"], tokens=p["tokens"], fee=fee, fee_mul=p["fee_mul"], r=p["r"])
+
+    return pool
--- curvesim/pool_data/__init__.py
+++ curvesim/pool_data/__init__.py
@@ -2,7 +2,7 @@ __all__ = ["from_address", "from_symbol", "get", "queries"]
 from curvesim.pool_data.queries import from_address, from_symbol
 
 
-def get(address_or_symbol, chain, src="cg", balanced=(True, True), days=60):
+def get(address_or_symbol, chain="mainnet", src="cg", balanced=(True, True), days=60):
     if address_or_symbol.startswith("0x"):
         from_x = from_address
     else:
