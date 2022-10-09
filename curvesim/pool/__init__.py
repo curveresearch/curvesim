b7cb98090d94942e91b24c964aaf3e6ba770d47a
nagakingg <nagakingg@protonmail.com>
2022-10-09 13:47:56 -0400

    Update pool.get

--- curvesim/pool/__init__.py
+++ curvesim/pool/__init__.py
@@ -8,7 +8,7 @@ from .pool import Pool
 make = Pool
 
 
-def get(address_or_symbol, chain="mainnet", src="cg", balanced=(True, True)):
+def get(address_or_symbol, chain="mainnet", src="cg", balanced=(False, False)):
     p = _get_pool_data(
         address_or_symbol,
         chain=chain,
@@ -16,15 +16,4 @@ def get(address_or_symbol, chain="mainnet", src="cg", balanced=(True, True)):
         balanced=balanced,
     )
 
-    if p["A_base"]:
-        A = [p["A"], p["A_base"]]
-        fee = [p["fee"], p["fee_base"]]
-    else:
-        A = p["A"]
-        fee = p["fee"]
-
-    pool = make(
-        A, p["D"], p["n"], tokens=p["tokens"], fee=fee, fee_mul=p["fee_mul"], r=p["r"]
-    )
-
-    return pool
+    return p.pool()
