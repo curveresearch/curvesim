0bf0e2e93dc293ea1daa3876406919f59de493d8
Chan-Ho Suh <chanhosuh@gmail.com>
2022-10-11 09:43:42 -0400

    Update jobs script

--- fraxbp_sim.py
+++ fraxbp_sim.py
@@ -4,26 +4,18 @@ import curvesim
 
 if __name__ == "__main__":
     t = time.time()
-    pools = [
-        "SUSDFRAXBP3CRV-f",
-        "GUSDFRAXBP3CRV-f",
-        "BUSDFRAXBP3CRV-f",
-        "TUSDFRAXBP3CRV-f",
-    ]
-    for poolname in pools:
+    pools = {
+        "SUSDFRAXBP3CRV-f": range(100, 700, 50),
+        "GUSDFRAXBP3CRV-f": range(100, 1700, 100),
+        "BUSDFRAXBP3CRV-f": range(100, 1700, 100),
+        "TUSDFRAXBP3CRV-f": range(100, 850, 50),
+    }
+    for poolname, A_list in pools.items():
+        res = curvesim.autosim(poolname, ncpu=4, src="nomics", fee=[0.0004], A=A_list)
+    for poolname, A_list in pools.items():
         res = curvesim.autosim(
-            poolname,
-            ncpu=4,
-            src="nomics",
-            A=range(100, 1700, 100),
-        )
-    for poolname in pools:
-        res = curvesim.autosim(
-            poolname,
-            ncpu=4,
-            src="nomics",
-            fee_base=0.0001,
-            A=range(100, 1700, 100),
+            poolname, ncpu=4, src="nomics", fee=[0.0004], fee_base=0.0001, A=A_list
         )
     elapsed = time.time() - t
-    print("Elapsed time:", elapsed)
+    minutes, seconds = divmod(elapsed, 60)
+    print(f"Elapsed time: {minutes}m, {seconds}s")
