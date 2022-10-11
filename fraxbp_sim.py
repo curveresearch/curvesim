a87de3326c0b5371a72f9b40c484a09cbd252205
Chan-Ho Suh <chanhosuh@gmail.com>
2022-10-10 21:35:29 -0400

    Script to run all pool jobs

--- fraxbp_sim.py
+++ fraxbp_sim.py
@@ -4,11 +4,26 @@ import curvesim
 
 if __name__ == "__main__":
     t = time.time()
-    # res = curvesim.autosim("3pool", test=True, ncpu=1)
-    # res = curvesim.autosim(
-    #     "crvFRAX", fee=[0.0001, 0.0002, 0.0003, 0.0004, 0.0005], ncpu=4
-    # )
-    res = curvesim.autosim("GUSDFRAXBP3CRV-f", ncpu=4)
-    # res = curvesim.autosim("SUSDFRAXBP3CRV-f", ncpu=4, src="nomics")
+    pools = [
+        "SUSDFRAXBP3CRV-f",
+        "GUSDFRAXBP3CRV-f",
+        "BUSDFRAXBP3CRV-f",
+        "TUSDFRAXBP3CRV-f",
+    ]
+    for poolname in pools:
+        res = curvesim.autosim(
+            poolname,
+            ncpu=4,
+            src="nomics",
+            A=range(100, 1700, 100),
+        )
+    for poolname in pools:
+        res = curvesim.autosim(
+            poolname,
+            ncpu=4,
+            src="nomics",
+            fee_base=0.0001,
+            A=range(100, 1700, 100),
+        )
     elapsed = time.time() - t
     print("Elapsed time:", elapsed)
