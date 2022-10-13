e98fd38c8a5f06b1162d0e14a4506794be85e62a
nagakingg <nagakingg@protonmail.com>
2022-10-13 13:34:01 -0400

    Add price_data submodule

---
+++ curvesim/price_data/__init__.py
@@ -0,0 +1,14 @@
+from .sources import coingecko, local, nomics
+
+
+def get(coins, days=60, data_dir="data", src="coingecko"):
+    if src == "coingecko":
+        prices, volumes, pzero = coingecko(coins, days=days)
+
+    elif src == "nomics":
+        prices, volumes, pzero = nomics(coins, days=days)
+
+    elif src == "local":
+        prices, volumes, pzero = local(coins, data_dir=data_dir)
+
+    return prices, volumes, pzero
