ea16c011c3a79250a4b2e1a5f1230b54ab7c68a4
Chan-Ho Suh <chanhosuh@gmail.com>
2022-10-13 19:40:34 -0400

    Add actual sim run to workflow

---
+++ .github/workflows/run_sim_request.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import json
+import sys
+
+sys.path.insert(0, "../")
+import curvesim  # noqa
+
+json_data = sys.stdin.read()
+pool_settings = json.loads(json_data)
+
+poolname = pool_settings["poolname"]
+chain = pool_settings["chain"]
+
+res = curvesim.autosim(poolname, chain=chain, ncpu=4)
