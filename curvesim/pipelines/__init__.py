b8102fe16bb1b3732b30c79949a1f2b9bf71d70f
nagakingg <nagakingg@protonmail.com>
2022-11-04 11:24:51 -0400

    Fix cross ref in pipelines.__init__ doc

--- curvesim/pipelines/__init__.py
+++ curvesim/pipelines/__init__.py
@@ -4,7 +4,7 @@ Tools for implementing and running simulation pipelines.
 Pipelines iterate over pools with parameters set by
 :mod:`.param_samplers` and time-series data produced by :mod:`.price_samplers`.
 Each pipeline implements a "strategy" dictating what is done at each timestep
-(e.g., see :func:`pipelines.arbitrage.strategy`).
+(e.g., see :func:`.pipelines.arbitrage.strategy`).
 
 Typically, a pipeline takes in :class:`pool_data`; specifies a param_sampler,
 price_sampler, and strategy; and returns metrics/results.
