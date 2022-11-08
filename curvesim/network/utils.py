b6924faaea4041216915afcecb97084d02e6047f
Chan-Ho Suh <chanhosuh@gmail.com>
2022-11-08 18:51:23 -0500

    Attempt to fix jupyer running event loop error
    Using `run_until_complete` in jupyter notebook will throw
    a loop is running error.  So the fix is to check if loop is
    running and then do alternative logic.  Current fix doesn't
    actually work but times out.
    
    Other changes:
    - wrap decorator using functools
    - check for running event loop

--- curvesim/network/utils.py
+++ curvesim/network/utils.py
@@ -1,4 +1,6 @@
 import asyncio
+import concurrent
+import functools
 
 from gmpy2 import mpz
 
@@ -39,9 +41,24 @@ def sync(func):
         Sync version of the async function.
     """
 
+    @functools.wraps(func)
     def inner(*args, event_loop=None, **kwargs):
         loop = event_loop or asyncio.get_event_loop()
-        res = loop.run_until_complete(func(*args, **kwargs))
+        coro = func(*args, **kwargs)
+        if loop.is_running():
+            try:
+                future = asyncio.run_coroutine_threadsafe(coro, loop)
+                res = future.result(timeout=60)
+            except concurrent.futures.TimeoutError as e:
+                print("The coroutine took too long, cancelling the task...")
+                future.cancel()
+                raise e
+            except Exception as e:
+                print("The coroutine raised an exception: {!r}".format(e))
+                raise e
+        else:
+            res = loop.run_until_complete(coro)
+
         return res
 
     return inner
