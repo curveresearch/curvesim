b540df15a2d77286d2ea535a49a10faa584bb7b5
Chan-Ho Suh <chanhosuh@gmail.com>
2022-11-08 13:08:24 -0500

    Flesh out pool base class

--- curvesim/pool/base.py
+++ curvesim/pool/base.py
@@ -4,4 +4,40 @@ Base class for any pool-like entity.
 
 
 class Pool:
-    pass
+    @property
+    def name(self):
+        """Descriptive name for this pool"""
+        if hasattr(self, "metadata"):
+            return self.metadata["name"]
+        return "Custom pool"
+
+    @property
+    def address(self):
+        """Address on chain"""
+        if hasattr(self, "metadata"):
+            return self.metadata["address"]
+        return "0x" + "0" * 40
+
+    @property
+    def chain(self):
+        """Chain for this pool"""
+        if hasattr(self, "metadata"):
+            return self.metadata["chain"]
+        return "No chain"
+
+    @property
+    def pool_type(self):
+        """Type of this pool"""
+        if hasattr(self, "metadata"):
+            return self.metadata["pool_type"]
+        return "Custom"
+
+    @property
+    def symbol(self):
+        """LP token symbol"""
+        if hasattr(self, "metadata"):
+            return self.metadata["symbol"]
+        return "Custom"
+
+    def __repr__(self):
+        return f"<{self.__class__.__name__} address={self.address} chain={self.chain}>"
