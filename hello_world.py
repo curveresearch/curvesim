adf8eb3284ae75888d34257cf935ed23127a6642
Chan-Ho Suh <chanhosuh@gmail.com>
2022-11-10 13:57:13 -0500

    Remove deprecated script

--- hello_world.py
+++
@@ -1,9 +0,0 @@
-import time
-
-import curvesim
-
-if __name__ == "__main__":
-    t = time.time()
-    res = curvesim.autosim("3pool", test=True, ncpu=1)
-    elapsed = time.time() - t
-    print("Elapsed time:", elapsed)
