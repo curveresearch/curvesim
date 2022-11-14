78fa4a8bda187ed70cc507b9aced51682a91cc6f
nagakingg <nagakingg@protonmail.com>
2022-11-14 18:55:22 -0500

    Bump version: 0.3.0.a2 → 0.3.0.rc0

--- curvesim/version.py
+++ curvesim/version.py
@@ -4,7 +4,7 @@ Canonical source of truth for version info.
 The version number should not be changed manually here.  Instead,
 a tool such as `bump2version` should be used.
 """
-__version__ = "0.3.0.a2"
+__version__ = "0.3.0.rc0"
 # FIXME: this logic isn't quite correct here for pre-release versions
 __version_info__ = tuple(
     int(num) if num.isdigit() else num for num in __version__.split(".")
