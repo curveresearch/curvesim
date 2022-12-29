3beb2c6632e7aa1364f3c5475b95601a1bfb8945
Chan-Ho Suh <chanhosuh@gmail.com>
2022-12-29 17:01:44 -0500

    Remove unneeded method from sim rai pool

--- curvesim/pool/sim_interface/raipool.py
+++ curvesim/pool/sim_interface/raipool.py
@@ -1,10 +1,7 @@
 from curvesim.pool.stableswap.raipool import CurveRaiPool
 
-from ..stableswap import functions as pool_functions
 from .metapool import SimCurveMetaPool
 
 
 class SimCurveRaiPool(CurveRaiPool, SimCurveMetaPool):
-    @property
-    def pricing_fns(self):
-        return (pool_functions.dydx_metapool_rai, pool_functions.dydx_rai)
+    """Sim interface for Curve RAI metapool"""
