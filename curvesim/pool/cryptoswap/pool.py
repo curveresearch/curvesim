0187c86923f75e56bed9c21fdd5bfcced5558159
Chan-Ho Suh <chanhosuh@gmail.com>
2023-01-17 17:30:40 -0500

    Start python v2 pool; add factory contract

---
+++ curvesim/pool/cryptoswap/pool.py
@@ -0,0 +1,55 @@
+import time
+
+
+def get_unix_timestamp():
+    """Get the timestamp in Unix time."""
+    return int(time.time())
+
+
+class CurveCryptoPool:
+    def __init__(
+        self,
+        A: int,
+        gamma: int,
+        mid_fee: int,
+        out_fee: int,
+        allowed_extra_profit: int,
+        fee_gamma: int,
+        adjustment_step: int,
+        admin_fee: int,
+        ma_half_time: int,
+        initial_price: int,
+        token: str,
+        coins,
+        precisions: int,
+    ):
+        # Pack A and gamma:
+        # shifted A + gamma
+        A_gamma: int = A << 128
+        A_gamma = A_gamma | gamma
+        self.initial_A_gamma = A_gamma
+        self.future_A_gamma = A_gamma
+
+        self.mid_fee = mid_fee
+        self.out_fee = out_fee
+        self.allowed_extra_profit = allowed_extra_profit
+        self.fee_gamma = fee_gamma
+        self.adjustment_step = adjustment_step
+        self.admin_fee = admin_fee
+
+        self.price_scale = initial_price
+        self._price_oracle = initial_price
+        self.last_prices = initial_price
+        self.last_prices_timestamp = get_unix_timestamp()
+        self.ma_half_time = ma_half_time
+
+        self.xcp_profit_a = 10**18
+
+        self.token = token
+        self.coins = coins
+        self.PRECISIONS = precisions
+
+        if len(coins) != len(precisions):
+            raise ValueError("`coins` must have same length as `precisions`")
+
+        self.n = len(coins)
