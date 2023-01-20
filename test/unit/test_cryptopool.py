0e5fa5d7469535cc3b22bc6b80a6daa76ffae4bd
Chan-Ho Suh <chanhosuh@gmail.com>
2023-01-20 16:54:46 -0500

    Create newton_D unit test

--- test/unit/test_cryptopool.py
+++ test/unit/test_cryptopool.py
@@ -5,58 +5,43 @@ from hypothesis import strategies as st
 from curvesim.pool import CurveCryptoPool
 
 
-def initialize_pool(vyper_pool):
+def initialize_pool(vyper_cryptopool):
     """
     Initialize python-based pool from the state variables of the
     vyper-based implementation.
     """
-    A = vyper_pool.A()
-    n_coins = vyper_pool.N_COINS()
-    balances = [vyper_pool.balances(i) for i in range(n_coins)]
-    rates = [vyper_pool.rates(i) for i in range(n_coins)]
-    lp_total_supply = vyper_pool.totalSupply()
-    fee = vyper_pool.fee()
-    admin_fee = vyper_pool.admin_fee()
+    A = vyper_cryptopool.A()
+    gamma = vyper_cryptopool.gamma()
+    n_coins = vyper_cryptopool.N_COINS()
+    balances = [vyper_cryptopool.balances(i) for i in range(n_coins)]
+    precisions = vyper_cryptopool.eval("self._get_precisions()")
+    lp_total_supply = vyper_cryptopool.totalSupply()
+    mid_fee = vyper_cryptopool.mid_fee()
+    out_fee = vyper_cryptopool.out_fee()
+    allowed_extra_profit = vyper_cryptopool.allowed_extra_profit()
+    fee_gamma = vyper_cryptopool.fee_gamma()
+    adjustment_step = vyper_cryptopool.adjustment_step()
+    admin_fee = vyper_cryptopool.admin_fee()
+    ma_half_time = vyper_cryptopool.ma_half_time()
+    price_scale = vyper_cryptopool.price_scale()
+
     pool = CurveCryptoPool(
-        A,
+        A=A,
+        gamma=gamma,
         D=balances,
         n=n_coins,
-        rates=rates,
+        precisions=precisions,
         tokens=lp_total_supply,
-        fee=fee,
+        mid_fee=mid_fee,
+        out_fee=out_fee,
+        allowed_extra_profit=allowed_extra_profit,
+        fee_gamma=fee_gamma,
+        adjustment_step=adjustment_step,
         admin_fee=admin_fee,
+        ma_half_time=ma_half_time,
+        initial_price=price_scale,
     )
     return pool
-
-
-def test_get_D_against_prod(vyper_3pool, mainnet_3pool_state):
-    """
-    Test boa value against live contract.
-
-    This checks boa is working correctly and also ensures mainnet
-    state stays consistent.
-    """
-    # Compare against virtual price since that's exposed externally
-    # while `get_D` is internal in the contract.
-    D = vyper_3pool.D()
-    total_supply = mainnet_3pool_state["lp_tokens"]
-    virtual_price = D * 10**18 // total_supply
-
-    expected_virtual_price = mainnet_3pool_state["virtual_price"]
-    assert virtual_price == expected_virtual_price
-
-
-def test_get_D_mainnet(vyper_3pool):
-    """
-    Test D calculation against vyper implementation using
-    mainnet state.
-    """
-    expected_D = vyper_3pool.D()
-
-    python_3pool = initialize_pool(vyper_3pool)
-    D = python_3pool.D()
-
-    assert D == expected_D
 
 
 # We can assume the contract works on more extreme values; we only need
@@ -74,17 +59,24 @@ positive_balance = st.integers(min_value=10**5 * D_UNIT, max_value=10**10 * D_UN
     max_examples=5,
     deadline=None,
 )
-def test_get_D(vyper_3pool, x0, x1, x2):
+def test_newton_D(vyper_cryptopool, x0, x1, x2):
     """Test D calculation against vyper implementation."""
 
     _balances = [x0, x1, x2]
-    p = [vyper_3pool.rates(i) for i in range(len(_balances))]
-    balances = [x * 10**18 // p for x, p in zip(_balances, p)]
+    precisions = vyper_cryptopool.eval("self._get_precisions()")
+    balances = [x // p for x, p in zip(_balances, precisions)]
 
-    vyper_3pool.eval(f"self.balances={balances}")
-    expected_D = vyper_3pool.D()
+    vyper_cryptopool.eval(f"self.balances={balances}")
+    xp = vyper_cryptopool.eval("self.xp()")
+    xp = list(xp)
+    A = vyper_cryptopool.A()
+    gamma = vyper_cryptopool.gamma()
+    expected_D = vyper_cryptopool.eval(f"self.newton_D({A}, {gamma}, {xp})")
 
-    pool = initialize_pool(vyper_3pool)
-    D = pool.D()
+    pool = initialize_pool(vyper_cryptopool)
+    xp = pool._xp()  # pylint: disable=protected-access
+    A = pool.A
+    gamma = pool.gamma
+    D = pool._newton_D(A, gamma, xp)
 
     assert D == expected_D
