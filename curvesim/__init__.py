8efb41c946885c1db7b8e9c8e16d516aff166532
Chan-Ho Suh <chanhosuh@gmail.com>
2023-03-04 14:46:49 -0500

    Disable simd extensions for numpy

--- curvesim/__init__.py
+++ curvesim/__init__.py
@@ -1,13 +1,12 @@
 """Package to simulate Curve pool."""
 __all__ = ["autosim", "bonding_curve", "order_book", "__version__", "__version_info__"]
 
-import numpy
 
 from ._bonding_curve import bonding_curve
 from ._order_book import order_book
 from .sim import autosim
 from .version import __version__, __version_info__
 
-print(numpy.show_config())
-
+# import numpy
+# print(numpy.show_config())
 # print("OPENBLAS_CORETYPE:", os.environ["OPENBLAS_CORETYPE"])
