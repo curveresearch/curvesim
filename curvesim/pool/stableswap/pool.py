f8b360d9e9eaa28a27cb40630b4215ef23e01c72
Chan-Ho Suh <chanhosuh@gmail.com>
2023-03-06 17:54:56 -0500

    Revert temp changes; don't use fee on depth calc

--- curvesim/pool/stableswap/pool.py
+++ curvesim/pool/stableswap/pool.py
@@ -604,22 +604,13 @@ class CurvePool(Pool):  # pylint: disable=too-many-instance-attributes
             xi * (xj * A_pow * x_prod + D_pow)
         )
 
-        # if use_fee:
-        #     if self.fee_mul is None:
-        #         fee_factor = self.fee / 10**10
-        #     else:
-        #         fee_factor = self.dynamic_fee(xi, xj) / 10**10
-        # else:
-        #     fee_factor = 0
         if use_fee:
             if self.fee_mul is None:
                 fee_factor = self.fee / 10**10
             else:
-                dx = 10**12
-                fee_factor = (
-                    self.dynamic_fee(xi + dx // 2, xj - int(dydx * dx) // 2) / 10**10
-                )
-        # fee_factor = 0
+                fee_factor = self.dynamic_fee(xi, xj) / 10**10
+        else:
+            fee_factor = 0
 
         dydx *= 1 - fee_factor
 
