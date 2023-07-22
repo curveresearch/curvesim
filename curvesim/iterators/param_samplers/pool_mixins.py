7c141ff33139d53ed5e3731a7a5299dc00d1cf20
Chan-Ho Suh <chanhosuh@gmail.com>
2023-07-22 16:38:51 -0400

    Add docstrings

--- curvesim/iterators/param_samplers/pool_mixins.py
+++ curvesim/iterators/param_samplers/pool_mixins.py
@@ -1,4 +1,11 @@
-from curvesim.pool.sim_interface import SimCurvePool, SimCurveMetaPool
+"""
+These mixins extend Curve pool classes for the purpose of generating iterables
+of pool instances configured by different simulation parameters.
+
+Each mixin defines a different pool type and a set of special attribute setters.
+"""
+
+from curvesim.pool.sim_interface import SimCurveMetaPool, SimCurvePool
 
 
 class CurvePoolMixin:
@@ -13,6 +20,12 @@ class CurvePoolMixin:
 
     @property
     def setters(self):
+        """
+        Returns
+        -------
+        dict
+            A dictionary containing the special setters for the pool parameters.
+        """
         return {"D": stableswap_D_to_balances}
 
 
@@ -28,6 +41,12 @@ class CurveMetaPoolMixin:
 
     @property
     def setters(self):
+        """
+        Returns
+        -------
+        dict
+            A dictionary containing the special setters for the pool parameters.
+        """
         return {"D": stableswap_D_to_balances, "D_base": stableswap_D_base_to_balances}
 
 
@@ -43,16 +62,42 @@ class CurveCryptoPoolMixin:
 
     @property
     def setters(self):
+        """
+        Returns
+        -------
+        dict
+            A dictionary containing the special setters for the pool parameters.
+        """
         return {"D": cryptoswap_D_to_balances}
 
 
 def stableswap_D_to_balances(pool, D):
+    """
+    Sets the balance for each token in the pool based on the provided
+    invariant value.
+
+    Parameters
+    ----------
+    pool : instance of _pool_type
+    D : int
+        The invariant value.
+    """
     rates = pool.rates
     n = pool.n
     pool.balances = [D // n * 10**18 // r for r in rates]
 
 
 def stableswap_D_base_to_balances(pool, D_base):
+    """
+    Sets the balance for each token in the basepool based on the provided
+    invariant value.
+
+    Parameters
+    ----------
+    pool : instance of _pool_type
+    D_base : int
+        The invariant value for the basepool.
+    """
     basepool = pool.basepool
     rates = basepool.rates
     n = basepool.n
@@ -60,6 +105,16 @@ def stableswap_D_base_to_balances(pool, D_base):
 
 
 def cryptoswap_D_to_balances(pool, D):
+    """
+    Sets the balance for each token in the pool based on the provided
+    invariant value.
+
+    Parameters
+    ----------
+    pool : instance of _pool_type
+    D : int
+        The invariant value.
+    """
     n = pool.n
     price_scale = pool.price_scale
     precisions = pool.precisions
