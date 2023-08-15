56e9c8a7a5bc81ccf876249937a38976cdaaf4fb
Chan-Ho Suh <chanhosuh@gmail.com>
2023-08-15 12:38:25 -0400

    Fix the fix

--- test/simple_ci.py
+++ test/simple_ci.py
@@ -85,9 +85,6 @@ def per_run(sim, stored):
     print("Testing per-run data...")
 
     # Compare metric columns
-    stored = stored.drop(
-        columns=[("liquidity_density", "median"), ("liquidity_density", "min")]
-    )
     compare_metrics(sim.columns, stored.columns)
     sim = sim[stored.columns]
 
@@ -119,9 +116,7 @@ def per_trade(sim, stored, threshold=0.9):
     print("Testing per-trade data...")
 
     # Compare metric columns
-    stored = stored.drop(
-        columns=[("liquidity_density", "median"), ("liquidity_density", "min")]
-    )
+    stored = stored.drop(columns=["liquidity_density"])
     compare_metrics(sim.columns, stored.columns)
     sim = sim[stored.columns]
 
