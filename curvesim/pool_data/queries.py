f8b6da592a939917ba3a34033c2e07e86dd285ba
Chan-Ho Suh <chanhosuh@gmail.com>
2023-08-15 07:19:52 -0400

    Add needed default arg value

--- curvesim/pool_data/queries.py
+++ curvesim/pool_data/queries.py
@@ -4,7 +4,7 @@ from ..network.subgraph import pool_snapshot_sync, symbol_address_sync
 from ..network.web3 import underlying_coin_info_sync
 
 
-def from_address(address, chain, env, end_ts=None):
+def from_address(address, chain, env="prod", end_ts=None):
     """
     Returns
 
@@ -14,6 +14,8 @@ def from_address(address, chain, env, end_ts=None):
         Address prefixed with '0x'
     chain: str
         Chain name
+    env: str
+        Environment name for subgraph: 'prod' or 'staging'
 
     Returns
     -------
@@ -21,7 +23,7 @@ def from_address(address, chain, env, end_ts=None):
     :func:`curvesim.network.subgraph.pool_snapshot`.
     """
     loop = asyncio.get_event_loop()
-    data = pool_snapshot_sync(address, chain, env, end_ts=end_ts, event_loop=loop)
+    data = pool_snapshot_sync(address, chain, env=env, end_ts=end_ts, event_loop=loop)
 
     # Get underlying token addresses
     if data["pool_type"] == "LENDING":
