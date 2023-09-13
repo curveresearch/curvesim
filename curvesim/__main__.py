9e40ccc29c75733408f1d6c04975c20fa9e0ae3b
Chan-Ho Suh <chanhosuh@gmail.com>
2023-09-13 17:24:11 -0400

    Fix hello-world

--- curvesim/__main__.py
+++ curvesim/__main__.py
@@ -15,10 +15,14 @@ def hello_world():
     """Simple sim run as a health check."""
     # pylint: disable=redefined-outer-name
     t = time.time()
-    res = autosim("0xbEbc44782C7dB0a1A60Cb6fe97d0b483032FF1C7", test=True, ncpu=1)
+    res = autosim(
+        "0xbEbc44782C7dB0a1A60Cb6fe97d0b483032FF1C7",
+        A=[100, 1000],
+        fee=[3000000, 4000000],
+        ncpu=1,
+    )
     elapsed = time.time() - t
     print("Elapsed time:", elapsed)
-
     return res
 
 
