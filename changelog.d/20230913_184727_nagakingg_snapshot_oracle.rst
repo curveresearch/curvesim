3904231629ebc0248bdc5bd49c942e8d34cf97d3
naga <nagakingg@protonmail.com>
2023-09-15 11:42:07 -0400

    Add unit test for cryptopool snapshots

--- changelog.d/20230913_184727_nagakingg_snapshot_oracle.rst
+++ changelog.d/20230913_184727_nagakingg_snapshot_oracle.rst
@@ -1,5 +1,6 @@
 Fixed
 -----
 
-- Added _price_oracle to CurveCryptoPoolBalanceSnapshot
+- Added _price_oracle and xcp_profit_a to CurveCryptoPoolBalanceSnapshot
+- Added snapshot test for CurveCryptoPools to unit/test_snapshot
 
--- test/unit/test_snapshot.py
+++ test/unit/test_snapshot.py
@@ -85,6 +85,62 @@ def test_metapool_balance_snapshot():
     assert basepool.tokens == pre_lp_tokens
 
 
+def test_2_coin_cryptopool_balance_snapshot(sim_curve_crypto_pool):
+    _test_cryptool_balance_snapshot(sim_curve_crypto_pool)
+
+
+def test_tricrypto_balance_snapshot(sim_curve_tricrypto_pool):
+    _test_cryptool_balance_snapshot(sim_curve_tricrypto_pool)
+
+
+def _test_cryptool_balance_snapshot(pool):
+    snapshot = pool.get_snapshot()
+
+    pre_balances = pool.balances.copy()
+    pre_D = pool.D
+    pre_price_scale = pool.price_scale.copy()
+    pre_price_oracle = pool._price_oracle.copy()
+    pre_virtual_price = pool.virtual_price
+    pre_xcp_profit = pool.xcp_profit
+    pre_xcp_profit_a = pool.xcp_profit_a
+    pre_last_prices = pool.last_prices.copy()
+    pre_last_prices_timestamp = pool.last_prices_timestamp
+
+    # ---- test exchange ---- #
+    # Use two trades since last_prices == _price_oracle at start
+    pool._increment_timestamp(42)  # 504 seconds
+    pool.exchange(0, 1, 10**26 // 2)
+    pool._increment_timestamp(42)
+    pool.exchange(0, 1, 10**26 // 2)
+
+    # test that `exchange` changes expected values
+    assert pool.balances != pre_balances
+    assert pool.D != pre_D
+    assert pool.price_scale != pre_price_scale  # _tweak_price should update price_scale
+    assert pool._price_oracle != pre_price_oracle
+    assert pool.virtual_price != pre_virtual_price
+    assert pool.xcp_profit != pre_xcp_profit
+    assert pool.xcp_profit_a == pre_xcp_profit_a  # not claimed yet
+    assert pool.last_prices != pre_last_prices
+    assert pool.last_prices_timestamp != pre_last_prices_timestamp
+
+    # test _claim_admin_fees
+    pool._claim_admin_fees()
+    assert pool.xcp_profit_a != pre_xcp_profit_a
+
+    # Ensure all reverted
+    pool.revert_to_snapshot(snapshot)
+    assert pool.balances == pre_balances
+    assert pool.D == pre_D
+    assert pool.price_scale == pre_price_scale
+    assert pool._price_oracle == pre_price_oracle
+    assert pool.virtual_price == pre_virtual_price
+    assert pool.xcp_profit == pre_xcp_profit
+    assert pool.xcp_profit_a == pre_xcp_profit_a
+    assert pool.last_prices == pre_last_prices
+    assert pool.last_prices_timestamp == pre_last_prices_timestamp
+
+
 def test_snapshot_context():
     """Test context manager."""
     pool = SimCurvePool(A=250, D=1000000 * 10**18, n=2, admin_fee=5 * 10**9)
